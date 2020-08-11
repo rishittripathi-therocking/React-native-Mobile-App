f921cd0afc907d6869b1b0add7a12c424b97ecc3
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-08-11 13:12:35 +0530

    Social share

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite, addComment, postComment } from '../Redux/ActionCreaters';
 import * as Animatable from 'react-native-animatable';
+
+
 
 const mapStateToProps = state => {
     return {
@@ -73,6 +75,16 @@ function RenderDish(props) {
             }
         });
 
+        const shareDish = (title, message, url) =>{
+            Share.share({
+                title: title,
+                message: title + ': ' + message + ' ' + url,
+                url: url
+            }, {
+                dialogTitle: 'Share ' + title
+            });
+        }
+
         if (dish != null) {
             return(
                 <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
@@ -96,6 +108,14 @@ function RenderDish(props) {
                             type='font-awesome'
                             color='#512DA8'
                             onPress={() => props.onSelect()}
+                        />
+                        <Icon 
+                            raised
+                            reverse
+                            name='share'
+                            type="font-awesome"
+                            color='#512DA8'
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl+dish.image)}
                         />
                         
                     </Card>
